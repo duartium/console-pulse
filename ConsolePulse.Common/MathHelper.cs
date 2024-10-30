aafe1384e34bfcf036986a1ec06ea10d72d75da6
Byron Duarte <byronduarte95@gmail.com>
2024-10-30 16:31:20 -0500

    :1234:  feat: add math helper to calculates least common multiple & greatest common divisor

---
+++ ConsolePulse.Common/MathHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsolePulse.Common;
+
+public class MathHelper
+{
+
+    /// <summary>
+    /// Calculates the least common multiple (LCM) of two integers
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public int CalculateLeastCommonMultiple(int a, int b)
+    {
+        //convert to positive numbers
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+
+        int greatestCommonDivisor = CalculateGreatestCommonDivisor(a, b);
+
+        return (a * b) / greatestCommonDivisor;
+    }
+
+    /// <summary>
+    /// Calculates the Greatest Common Divisor (GCD) of two integers
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static int CalculateGreatestCommonDivisor(int a, int b)
+    {
+        while (b != 0) //until reduce
+        {
+            int temp = b;
+            b = a % b; //update res
+            a = temp;
+        }
+        return a;
+    }
+}
